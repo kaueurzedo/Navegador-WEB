7a1076ba79678c304d7e7696ad455136d007699e
Kaue Ignacio de Urzedo <kaueurzedo13@hotmail.com>
2023-01-09 22:14:26 -0300

    NAVEGADOR WEB
    NAVEGADOR WEB

---
+++ Navegador/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Navegador
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.btnvoltar = new System.Windows.Forms.Button();
+            this.btnavancar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txturl = new System.Windows.Forms.TextBox();
+            this.btnir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(-2, 31);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(783, 397);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Url = new System.Uri("https://www.google.com", System.UriKind.Absolute);
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // btnvoltar
+            // 
+            this.btnvoltar.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnvoltar.Location = new System.Drawing.Point(-2, 5);
+            this.btnvoltar.Name = "btnvoltar";
+            this.btnvoltar.Size = new System.Drawing.Size(101, 23);
+            this.btnvoltar.TabIndex = 1;
+            this.btnvoltar.Text = "VOLTAR";
+            this.btnvoltar.UseVisualStyleBackColor = true;
+            this.btnvoltar.Click += new System.EventHandler(this.btnvoltar_Click);
+            // 
+            // btnavancar
+            // 
+            this.btnavancar.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnavancar.Location = new System.Drawing.Point(105, 6);
+            this.btnavancar.Name = "btnavancar";
+            this.btnavancar.Size = new System.Drawing.Size(101, 23);
+            this.btnavancar.TabIndex = 2;
+            this.btnavancar.Text = "AVANÇAR";
+            this.btnavancar.UseVisualStyleBackColor = true;
+            this.btnavancar.Click += new System.EventHandler(this.btnavancar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(202, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = " URL";
+            // 
+            // txturl
+            // 
+            this.txturl.Location = new System.Drawing.Point(231, 5);
+            this.txturl.Name = "txturl";
+            this.txturl.Size = new System.Drawing.Size(473, 20);
+            this.txturl.TabIndex = 4;
+            // 
+            // btnir
+            // 
+            this.btnir.Location = new System.Drawing.Point(710, 2);
+            this.btnir.Name = "btnir";
+            this.btnir.Size = new System.Drawing.Size(75, 23);
+            this.btnir.TabIndex = 5;
+            this.btnir.Text = "IR";
+            this.btnir.UseVisualStyleBackColor = true;
+            this.btnir.Click += new System.EventHandler(this.btnir_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnir);
+            this.Controls.Add(this.txturl);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnavancar);
+            this.Controls.Add(this.btnvoltar);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "NAVEGADOR HACKER";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button btnvoltar;
+        private System.Windows.Forms.Button btnavancar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txturl;
+        private System.Windows.Forms.Button btnir;
+    }
+}
+
---
+++ Navegador/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Navegador
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            webBrowser1.Width = this.Width - 30;
+            webBrowser1.Height = this.Height - 122;
+        }
+
+        private void btnvoltar_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+
+        private void btnavancar_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            webBrowser1.Width = this.Width - 30;
+            webBrowser1.Height = this.Height - 122;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            webBrowser1.Dispose();
+        }
+
+        private void btnir_Click(object sender, EventArgs e)
+        {
+            var uri = new Uri(txturl.Text);
+            webBrowser1.Url = uri;
+        }
+
+        private void btnir_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
